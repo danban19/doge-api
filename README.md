1271b348f259b5a1dd090864366795ba914abf33
Daniel Banasik <daniel.banasik1@gmail.com>
2024-10-28 23:58:57 +0100

    imlement endpoint to fetch dog data, update deps and README

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # doge-api
 An API to retrieve data about dog breeds. Uses external API called "The Dog API".
+To get more info about the external API, explore the following documentation:
+https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=bOoHBz-8t
 
 # Python version
 The API uses Python 3.12.7 version. Use pyenv to install it according to this tutorial: https://realpython.com/intro-to-pyenv/
@@ -15,5 +17,4 @@ poetry install
 poetry shell
 
 # Run server
-uvicorn main:app
-
+uvicorn main:app --reload --env-file .env
--- src/main.py
+++ src/main.py
@@ -1,9 +1,47 @@
 """Entry point for the FastAPI application."""
+import os
+import requests
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/health")
 def read_root():
-    """Example endpoint returning a JSON object."""
-    return {"Hello": "World"}
+    """Endpoint to check the health of the application."""
+    return {"status": "ok"}
+
+@app.get("/breeds/")
+def get_breeds(limit: int = 10, page: int = 0):
+    """Endpoint to get all breeds from The Dog API.
+    https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=bOoHBz-8t
+    Args:
+        limit (int): Number of breeds to return.
+        page (int): Page number to return.
+    Returns:
+        dict: JSON response from The Dog API.
+    """
+    base_url = os.getenv("DOG_API_BASE_URL")
+    headers = {
+        "x-api-key": os.getenv("DOG_API_KEY")
+    }
+    url = f"{base_url}/breeds?limit={limit}&page={page}"
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+
+@app.get("/breeds/{breed_id}")
+def get_breed(breed_id: str):
+    """Endpoint to get a breed by ID from The Dog API.
+    Args:
+        breed_id (str): ID of the breed to return.
+    Returns:
+        dict: JSON response from The Dog API.
+    """
+    base_url = os.getenv("DOG_API_BASE_URL")
+    headers = {
+        "x-api-key": os.getenv("DOG_API_KEY")
+    }
+    url = f"{base_url}/breeds/{breed_id}"
+    response = requests.get(url, headers=headers)
+    return response.json()
